828adaa24ec1512ee8c73e0967dfcb0d2083e640
Noura <nouraellm@gmail.com>
2020-05-14 20:20:30 +0200

    Update howis.py
    Use isdigit() to check if the argv[1] is a number, the isinstance check will always fail because argv is a list. 
    You had an additional parenthesis in line 5 and it was causing an error.
    No need for paranthesis if it's only one condition.

--- bootcamp/bootcamp_python/day00/ex02/howis.py
+++ bootcamp/bootcamp_python/day00/ex02/howis.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 import sys
-if (len(sys.argv) == 2 and  isinstance(sys.argv[1], int)):
-	print(len((sys.argv[1]))
-	par = (int)(sys.argv[1])
-	if (par  == 0):
-		print("I'm Zero.")
-	elif (par % 2 == 0):
-		print("I'm Even.")
-	else:
-		print("I'm Odd.")
+
+if len(sys.argv) == 2 and sys.argv[1].isdigit():
+        print(len(sys.argv[1]))
+        par = int(sys.argv[1])
+        if par == 0:
+                print("I'm Zero.")
+        elif par % 2 == 0:
+                print("I'm Even.")
+        else:
+                print("I'm Odd.")
 else:
-	print("ERROR")	
+        print("ERROR")
